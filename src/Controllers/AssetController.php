d82ded215f5917b6d2d855eeec46e59e60813dcf
julien-alaria <julienalaria@gmail.com>
2026-08-12 19:25:18 +0200

    Fix

--- src/Controllers/AssetController.php
+++ src/Controllers/AssetController.php
@@ -8,7 +8,7 @@ class AssetController {
 
     public static function index(): void {
 
-        header('Content-Type: Application.json');
+        header('Content-Type: Application/json');
         echo json_encode(AssetModel::getAllTypes());
     }
 }
